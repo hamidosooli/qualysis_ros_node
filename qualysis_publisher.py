59d6b078a948c63e89286254458f18b95a602e86
Hamid Osooli <hamid.osooli@gmail.com>
2025-10-23 20:24:42 -0500

    Update qualysis_publisher.py

--- qualysis_publisher.py
+++ qualysis_publisher.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# code heavily adapted from https://github.com/tbretl/crazyflie-client/blob/main/ae483clients.py
 import rclpy
 from rclpy.node import Node
 
